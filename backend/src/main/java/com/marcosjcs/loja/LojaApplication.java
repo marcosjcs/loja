028dcef75e48b9c977b19ae546a75528e0a7ce8b
marcosjcs <jcs.marcos13@gmail.com>
2021-09-16 21:27:01 -0300

    Project created

---
+++ backend/src/main/java/com/marcosjcs/loja/LojaApplication.java
@@ -0,0 +1,13 @@
+package com.marcosjcs.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaApplication.class, args);
+	}
+
+}
